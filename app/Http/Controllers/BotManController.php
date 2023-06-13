47ae15e16fda5e5952ba8fedae67ffa12ade98dc
asrani <asrani.27@gmail.com>
2023-06-14 07:09:11 +0800

    f

--- app/Http/Controllers/BotManController.php
+++ app/Http/Controllers/BotManController.php
@@ -65,8 +65,6 @@ class BotManController extends Controller
                                             $this->say('Berhasil Di Update, Berikut Data Terbaru : ');
                                             $this->say('Tanggal : ' . $checkPresensi->tanggal . PHP_EOL . 'NIP : ' . $nip . PHP_EOL . 'Nama : ' . $checkNIP->nama . PHP_EOL . 'Jam Masuk : ' . Carbon::parse($checkPresensi->jam_masuk)->format('H:i') . PHP_EOL . 'Jam Pulang : ' . Carbon::parse($checkPresensi->jam_pulang)->format('H:i') . PHP_EOL . 'Telat : ' . ($checkPresensi->terlambat == null ? 0 . ' Menit' : $checkPresensi->terlambat . ' Menit') . PHP_EOL . 'Lebih Awal : ' . ($checkPresensi->lebih_awal == null ? 0  . ' Menit' : $checkPresensi->lebih_awal . ' Menit'));
                                         } elseif ($jawab == "PULANG" || $jawab == "pulang") {
-                                            $this->say($checkPresensi->id);
-
                                             $checkPresensi->update([
                                                 'jam_pulang' => $checkPresensi->tanggal . ' 07:' . rand(20, 30) . ':' . rand(1, 60),
                                                 'lebih_awal' => 0,
